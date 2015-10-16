f4cdef3092e3535be90f4d4aa4959ed5c39c37e6
Feodor Price <feodor_price@harvard.edu>
2015-10-15 20:53:44 -0400

    Updated the run_cmd_sorger_list.sh with minor changes

--- run_cmd_sorger_list.sh
+++ run_cmd_sorger_list.sh
@@ -1,5 +1,5 @@
 
-# guarantee our consisten defaults
+# guarantee our consistent defaults
 source new-modules.sh
 module load python/2.7.6-fasrc01 
 module load bwa/0.7.9a-fasrc01
@@ -10,6 +10,6 @@ module load samtools/1.2-fasrc01
 source activate PYTHON_DGE
 
 
-python ./dge-prod/Scripts/run_DGE_analysis.py --short_slurm_queue "short" --long_slurm_queue "long" --loose_barcodes --cleanup sample_map.txt Human Trugrade_384_set1 /n/regal/rubin_lab/fprice/DGE_processing/Alignment DGE_July15
+python ./dge-prod/Scripts/run_DGE_analysis.py --short_slurm_queue "short" --long_slurm_queue "long" --loose_barcodes --cleanup sample_map.txt Human Trugrade_384_set1 /n/regal/rubin_lab/fprice/DGE_processing/Alignment DGE_OCT15
 
 
