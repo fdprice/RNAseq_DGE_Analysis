ae5836fa097066b29d6f9e82dd55c95840d5a8a1
Bob Freeman <robertfreeman@g.harvard.edu>
2015-10-18 10:45:46 -0400

    fixed partition name in sbatch call

--- Scripts/SbatchController.py
+++ Scripts/SbatchController.py
@@ -173,7 +173,10 @@ class SbatchController:
 			print('Submitting ' + shell_script + ' to sbatch')
 			
 		script_basename = os.path.basename(shell_script)
-		cmd = 'sbatch -p ' + self.queue + ' -e ' + shell_script + '.stderr -o ' + shell_script + '.stdout'
+		# for now, hard-coding partition name. we only care about the partition from the calling code
+		#   in order to get the time length, not the partition name
+		#cmd = 'sbatch -p ' + self.queue + ' -e ' + shell_script + '.stderr -o ' + shell_script + '.stdout'
+		cmd = 'sbatch -p serial_requeue -e ' + shell_script + '.stderr -o ' + shell_script + '.stdout'
 
 		if self.memory:
 			cmd = cmd + ' --mem ' + str(self.memory*1024)
